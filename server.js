54009ba1de3e297f7fca72da33e0c11007bd50ac
Aleksey Klimenko <klimenkoaleksey.prog@gmail.com>
2022-06-04 19:38:53 +0300

    feat: edit dependecies

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ const middlewares = jsonServer.defaults({
 const PORT = process.env.PORT || 8000;
 
 server.use(middlewares);
-server.use(jsonServer.rewriter());
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+  })
+);
+
 server.use(router);
 
 server.listen(PORT, () => {
