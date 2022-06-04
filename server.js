0ed2a06884c421805d5915444a46709258281086
Aleksey Klimenko <klimenkoaleksey.prog@gmail.com>
2022-06-04 18:56:16 +0300

    feat: add Procfile and server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+import jsonServer from 'json-server';
+import path from 'path';
+const server = jsonServer.create();
+
+const router = jsonServer.router(
+  path.resolve(path.dirname(''), 'database', 'db.json')
+);
+
+const middlewares = jsonServer.defaults({
+  static: './build',
+});
+
+const PORT = process.env.PORT || 8000;
+
+server.use(middlewares);
+server.use(jsonServer.rewriter());
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log('server is running');
+});
